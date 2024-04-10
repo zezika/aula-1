ac2e46fb67013a69f1d2aca1305083d09f1c32a0
zezika <166394335+zezika@users.noreply.github.com>
2024-04-10 10:35:42 -0300

    atividade do node

--- aula3/HelloWorld.js/helloworld.js
+++ aula3/HelloWorld.js/helloworld.js
@@ -1,11 +1,12 @@
 const readline = require('readline');
 
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 rl.question('qual é o seu nome?', (nome) => {
-    console.log('Olá, ${nome} Bem vindo ao Node.js');
+    console.log(`Olá, ${nome} Bem vindo ao Node.js`);
     rl.close();   
 
 });   
